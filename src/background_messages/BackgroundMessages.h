fde02daf87eb18f1a8b8df54303319866f85a1c2
Jens Dede <jd@comnets.uni-bremen.de>
2018-07-13 11:36:47 +0200

    Background message definitions

---
+++ src/background_messages/BackgroundMessages.h
@@ -0,0 +1,16 @@
+/*
+ * BackgroundMessages.h
+ *
+ *  Created on: Jul 12, 2018
+ *      Author: jd
+ */
+
+#ifndef BACKGROUND_MESSAGES_BACKGROUNDMESSAGES_H_
+#define BACKGROUND_MESSAGES_BACKGROUNDMESSAGES_H_
+
+
+#include "BackgroundTypeDefinitions.h"
+#include "BackgroundEventMessage_m.h"
+
+
+#endif /* BACKGROUND_MESSAGES_BACKGROUNDMESSAGES_H_ */
