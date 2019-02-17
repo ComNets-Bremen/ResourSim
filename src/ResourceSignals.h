3d0d42375c895384da7b7bd4b97b23f5d21949a6
Jens Dede <jd@comnets.uni-bremen.de>
2019-02-17 14:30:06 +0100

    Add signals fired in case of critical events

---
+++ src/ResourceSignals.h
@@ -0,0 +1,14 @@
+/*
+ * ResourceSignals.h
+ *
+ *  Created on: Feb 15, 2019
+ *      Author: jd
+ */
+
+#ifndef RESOURCESIGNALS_H_
+#define RESOURCESIGNALS_H_
+
+#define BATTERY_PERCENTAGE_SIGNAL "resourcesim_battery_percentage_signal"
+#define BATTERY_INCONVENIENT_SIGNAL   "resourcesim_battery_inconvenient_signal"
+
+#endif /* RESOURCESIGNALS_H_ */
