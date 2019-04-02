0adbb540b0229377308ae6ffcaf893cfc94ceca8
Jens Dede <jd@comnets.uni-bremen.de>
2019-04-02 18:08:17 +0200

    Add emitted signals

--- src/ResourceSignals.h
+++ src/ResourceSignals.h
@@ -11,5 +11,7 @@
 #define BATTERY_PERCENTAGE_SIGNAL       "resourcesim_battery_percentage_signal"
 #define BATTERY_INCONVENIENT_SIGNAL     "resourcesim_battery_inconvenient_signal"
 #define CALCULATE_BATTERY_DIFFS         "resourcesim_battery_recalculate"
+#define WIFI_STATUS_UPDATE_SIGNAL       "resourcesim_wifi_status_update_signal"
+#define SCREEN_STATUS_UPDATE_SIGNAL     "resourcesim_screen_status_update_signal"
 
 #endif /* RESOURCESIGNALS_H_ */
