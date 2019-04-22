9235b4a166c36fadb33a71c06e53c093bd84ea5e
Jens Dede <jd@comnets.uni-bremen.de>
2019-04-22 17:07:59 +0200

    Run user simulation

--- runcmd.sh
+++ runcmd.sh
@@ -3,5 +3,7 @@
 # Run the general configuration using the command line environment and redirect
 # the output to a logfile
 
+#SIMTYPE="BucketBattery-WiFi-CPU-Background"
+SIMTYPE="UserOptimizationUser1"
 cd simulations
-../Resoursim -m -u Cmdenv -n .:../src --image-path=../images omnetpp.ini
+../Resoursim -m -u Cmdenv -c $SIMTYPE -n .:../src --image-path=../images omnetpp.ini
