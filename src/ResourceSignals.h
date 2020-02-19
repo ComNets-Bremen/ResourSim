6b7636a5b66350a34fef060a3513325815bf65ae
Jens Dede <jd@comnets.uni-bremen.de>
2020-02-19 12:02:37 +0100

    New signal: Battery state changed (chg -> dischg)

--- src/ResourceSignals.h
+++ src/ResourceSignals.h
@@ -10,6 +10,7 @@
 
 #define BATTERY_PERCENTAGE_SIGNAL       "resourcesim_battery_percentage_signal"
 #define BATTERY_INCONVENIENT_SIGNAL     "resourcesim_battery_inconvenient_signal"
+#define BATTERY_STATE_CHANGED_SIGNAL    "resourcesim_battery_state_changed_signal"
 #define CALCULATE_BATTERY_DIFFS         "resourcesim_battery_recalculate"
 #define WIFI_STATUS_UPDATE_SIGNAL       "resourcesim_wifi_status_update_signal"
 #define SCREEN_STATUS_UPDATE_SIGNAL     "resourcesim_screen_status_update_signal"
